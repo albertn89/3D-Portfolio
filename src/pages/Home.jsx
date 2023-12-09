8409a58fe7024df90bf7166bef2dd205b75ffbbe
Albert Negoro <56742226+albertn89@users.noreply.github.com>
2023-12-08 23:19:21 -0700

    finish adding island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 {
 	/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -31,12 +32,11 @@ const Home = () => {
 		<section className="w-full h-screen relative">
 			<Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
 				<Suspense fallback={<Loader />}>
-					<directionalLight />
-					<ambientLight />
-					<pointLight />
-					<spotLight />
-					<hemisphereLight />
+					<directionalLight position={[1, 1, 1]} intensity={2} />
+					<ambientLight intensity={0.5} />
+					<hemisphereLight skyColor="#B1E1FF" groundColor="#000000" intensity={1} />
 
+					<Sky />
 					<Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
 				</Suspense>
 			</Canvas>
